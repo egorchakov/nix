28ea254837c295b8dfa5d8021846345f8f498a78
Evgenii Gorchakov <evgorchakov@gmail.com>
2025-11-07 11:49:46 +0100

    sioyek

--- modules/home/gui.nix
+++ modules/home/gui.nix
@@ -11,6 +11,18 @@
         shell-integration-features = "sudo,ssh-env,ssh-terminfo";
       };
     };
+    sioyek = {
+      enable = true;
+      config = {
+        case_sensitive_search = "0";
+        default_dark_mode = "1";
+        dark_mode_background_color = "0.0 0.0 0.0";
+        font_size = "20";
+        prerender_next_page_presentation = "1";
+        should_launch_new_window = "1";
+        super_fast_search = "1";
+      };
+    };
   };
 
   stylix.targets.ghostty.enable = true;
