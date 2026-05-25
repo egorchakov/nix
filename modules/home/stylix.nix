abe4abbc59cd1b194ae09b20d6e21ae3bdca2f50
Evgenii Gorchakov <evgorchakov@gmail.com>
2026-05-25 20:57:53 +0200

    use iosevka-bin

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -25,7 +25,7 @@
     base16Scheme = "${pkgs.base16-schemes}/share/themes/evenok-dark.yaml";
     fonts = {
       monospace = {
-        package = pkgs.iosevka;
+        package = pkgs.iosevka-bin;
         name = "Iosevka";
       };
     };
