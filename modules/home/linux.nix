30dfaa4a258d972ce0c556f2624cb354fd2ee8ad
Evgenii Gorchakov <evgorchakov@gmail.com>
2026-05-29 17:15:33 +0200

    systemctl-tui

--- modules/home/linux.nix
+++ modules/home/linux.nix
@@ -1,8 +1,10 @@
-{ user, ... }:
+{ pkgs, user, ... }:
 {
   home = {
     username = user;
     homeDirectory = "/home/${user}";
     sessionVariables.NIXOS_OZONE_WL = "1";
+
+    packages = with pkgs; [ systemctl-tui ];
   };
 }
