f0c0054e3e6a3f74d31cafd5b9c082a288c5d205
Evgenii Gorchakov <evgorchakov@gmail.com>
2026-09-09 17:31:40 +0200

    herdr ssh

--- modules/home/linux.nix
+++ modules/home/linux.nix
@@ -5,17 +5,31 @@
     sessionVariables.NIXOS_OZONE_WL = "1";
 
     packages = with pkgs; [ systemctl-tui ];
+
+    # Herdr's remote attach does not start an interactive shell.
+    file.".ssh/rc".text = ''
+      if [ -S "$SSH_AUTH_SOCK" ]; then
+        ${pkgs.coreutils}/bin/ln -sfn "$SSH_AUTH_SOCK" "$HOME/.ssh/forwarded-agent.sock"
+      fi
+
+      # sshd also delegates X11 authentication to this hook.
+      if [ -n "$DISPLAY" ] && read -r proto cookie; then
+        case "$DISPLAY" in
+          localhost:*) display="unix:''${DISPLAY#localhost:}" ;;
+          *) display="$DISPLAY" ;;
+        esac
+        ${pkgs.xauth}/bin/xauth -q add "$display" "$proto" "$cookie"
+      fi
+    '';
   };
 
   programs.nushell.extraEnv = ''
-    if $nu.is-interactive and ($env.SSH_CONNECTION? != null) and ($env.SSH_AUTH_SOCK? != null) {
+    if $nu.is-interactive and (($env.SSH_CONNECTION? != null) or ($env.HERDR_ENV? != null) or ($env.ZELLIJ? != null)) {
       let forwarded_agent = $env.HOME | path join ".ssh" "forwarded-agent.sock"
 
-      if ($env.ZELLIJ? == null) and ($env.HERDR_ENV? == null) and ($env.SSH_AUTH_SOCK != $forwarded_agent) {
-        ${pkgs.coreutils}/bin/ln -sfn $env.SSH_AUTH_SOCK $forwarded_agent
+      if ($forwarded_agent | path exists) {
+        $env.SSH_AUTH_SOCK = $forwarded_agent
       }
-
-      $env.SSH_AUTH_SOCK = $forwarded_agent
     }
   '';
 }
